93b8a403f660676eb291d261e076a2fa00d9432e
The Saioman <the.saioman@gmail.com>
2012-09-11 13:00:16 +0900

    Using the constant for char buffer sizes.-

--- master/master.c
+++ master/master.c
@@ -152,7 +152,7 @@ void coverage_mpi (int argc,
     //
     // prepare workers' commands and arguments
     //
-    int buff_size = 4096;
+    int buff_size = _CHAR_BUFFER_SIZE_;
     char    *worker_command = "run_worker.sh";
     char    *worker_program  [nworkers];
     char    *worker_arg      [nworkers];
