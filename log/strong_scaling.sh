8cb745d321b70c3812aafa98e4aab4ff1dc77fb5
The Saioman <the.saioman@gmail.com>
2012-10-14 15:37:53 +0900

    Simulation scripts upgraded to accept the hostfile as parameter.-

--- log/strong_scaling.sh
+++ log/strong_scaling.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-NTX=$1
-NP=$2
-NP_NODE=$3
-HOSTFILE=hostfile.local
+HFILE=$1
+NTX=$2
+NP=$3
+NP_NODE=$4
 
-if [ -n "${NTX}" ] && [ -n "${NP}" ] && [ -n "${NP_NODE}" ]; then
-	echo -e "localhost	slots=1\n" > ${HOSTFILE}
+if [ -n "${HFILE}" ] && [ -n "${NTX}" ] && [ -n "${NP}" ] && [ -n "${NP_NODE}" ]; then
+	echo -e "localhost	slots=1\n" > ${HFILE}
 	NODES=$( echo "${NP}/${NP_NODE}" | bc )
 	for h in $(grep -v '^#' ~/hosts.20121003 | head -n ${NODES}); 
 	do 
-		echo "${h}        slots=${NP_NODE}" >> ${HOSTFILE}
+		echo "${h}        slots=${NP_NODE}" >> ${HFILE}
 	done
 	NTX_NP=$( echo "${NTX}/${NP}" | bc )
 	TOTAL_NTX=$( echo "${NP} * ${NTX_NP}" | bc )
 	echo "Number of processors:	${NP}"
 	echo "Transmitters per processor:	${NTX_NP}"
 	echo "Processes per node:	${NP_NODE}"
-	$(./run_many.sh ${TOTAL_NTX})
+	$( ./run_many.sh ${HFILE} ${TOTAL_NTX} )
 else
-	echo "Usage:"
-	echo "	$0 [number of transmitters] [number of processors] [number of processes per node]"
+	echo -e "Usage:\t$0 [hostfile to use] [number of transmitters] [number of processors] [number of processes per node]"
+	echo "Runs the strong-scaling simulations over MPI."
 fi
--- run_mpi.sh
+++ run_mpi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TX=$1
-OUT=$2
-#CMD="mpirun -x LD_LIBRARY_PATH --mca btl_tcp_endpoint_cache 122880 --hostfile hostfile.local -n 1 r.coverage -p"
-CMD="mpirun --mca btl tcp,sm,self --hostfile hostfile.local -n 1 r.coverage -p"
+HFILE=$1
+TX=$2
+OUT=$3
+CMD="mpirun --mca btl tcp,sm,self --hostfile ${HFILE} -n 1 r.coverage -p"
 
-if [ -n "${TX}" ]; then
+if [ -n "${HFILE}" ] && [ -n "${TX}" ]; then
     CMD="${CMD} ini_file=$(pwd)/parameters.ini tx_ini_sections=${TX}"
     if [ -n "${OUT}" ]; then
         CMD="${CMD} output_raster=${OUT}"
@@ -13,7 +13,7 @@ if [ -n "${TX}" ]; then
     echo ${CMD} >&2 && ${CMD}
     echo "Process took $SECONDS sec"
 else
-    echo "Usage"
-    echo "  $0 [transmitter's section name in the INI file] [output raster name]"
+    echo -e "Usage:\t$0 [hostfile to use] [transmitter's section name in the INI file] [output raster name]"
+    echo "Runs the radio coverage calculation prediction over MPI."
     echo
 fi
