cd20021c26fec6822231405b1882d01ee931f709
The Saioman <the.saioman@gmail.com>
2012-10-15 20:13:33 +0900

    Updated scripts for result plotting and analysis.-

--- log/best_time.sh
+++ log/best_time.sh
@@ -5,12 +5,13 @@ BEST_TIME=99999
 
 if [ $# -gt 1 ]; then
     for LOG in ${@:1}; do
-        TIME=$( cat ${LOG} | grep "Process took" | tr -d '[:alpha:]' | tr -d '[:blank:]' )
+        TIME=$( grep "Process took" ${LOG} | tr -d '[:alpha:]' | tr -d '[:blank:]' )
         if [ ${BEST_TIME} -gt ${TIME} ]; then
+            BEST_FILE=${LOG}
             BEST_TIME=${TIME}
         fi
     done
-    echo "${BEST_TIME}"
+    echo "${BEST_FILE}	${BEST_TIME}"
 else
     echo -e "Usage:\t $0 [log files ...]"
     echo "Extracts the best running time from a group of simulation results for weak scaling."
--- log/plot_weak_scaling.sh
+++ log/plot_weak_scaling.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 NTX=$( ./log_scale.sh 5 80 )
+NP=$( ./log_scale.sh 1 128 )
 
 for ntx in ${NTX}; do
     rm -f /tmp/${ntx}.dat
     touch /tmp/${ntx}.dat
-    for np in  $( ./log_scale.sh 1 128 ); do
-        BEST_TIME=$( ./best_time.sh weak_scaling_${ntx}_Tx/weak_scaling_${np}.*.txt )
+    for np in  ${NP}; do
+        BEST_TIME=$( ./best_time.sh weak_scaling_${ntx}_Tx/weak_scaling_${np}.*.txt | cut -f2 )
         echo "${np}	${BEST_TIME}" >> /tmp/${ntx}.dat
     done
 done
@@ -40,3 +41,41 @@ PLOT="$( echo "${PLOT}" | sed -e 's/, $//g' )"
 CMD="$( echo -e "${CMD}\n${PLOT}" )"
 echo "${CMD}"
 gnuplot -p -e "${CMD}"
+
+
+
+#################################
+# Relative processing time
+#
+#################################
+for ntx in ${NTX}; do 
+    ./discriminated_times.sh $( for np in ${NP}; do ./best_time.sh weak_scaling_${ntx}_Tx/weak_scaling_${np}.*.txt; done | cut -f1 ) | sort -n -k1 > /tmp/${ntx}.dat
+done
+
+for ntx in 5 20 80; do
+    CMD=$( cat <<EOF
+set term postscript eps enhanced; 
+set output "weak_scaling_relative_time_plot_${ntx}.eps";
+set title ".:. Weak scalability -- ${ntx} Transmitters .:.";
+set style data histograms;
+set style histogram rowstacked;
+set boxwidth 1 relative;
+set style fill pattern 3 border;
+set xlabel "Number of cores";
+set nolog x; 
+set ylabel "Relative processing time";
+set ytics 0.1;
+set yrange [0:1.20];
+set nolog y;
+EOF
+    )
+    PLOT="plot "
+    PLOT="$( echo "${PLOT}" | sed -e 's/, $//g' )"
+    PLOT="${PLOT} \"/tmp/${ntx}.dat\" using (\$2/\$6) title \"Dynamic worker-process spawning\", "
+    PLOT="${PLOT} \"/tmp/${ntx}.dat\" using (\$3/\$6):xticlabels(1) title \"Input data broadcasting\", "
+    PLOT="${PLOT} \"/tmp/${ntx}.dat\" using (\$4/\$6):xticlabels(1) title \"Processing loop\", "
+    PLOT="${PLOT} \"/tmp/${ntx}.dat\" using (\$5/\$6):xticlabels(1) title \"Create final coverage prediction\"; "
+    CMD="$( echo -e "${CMD}\n${PLOT}" )"
+    echo "${CMD}"
+    gnuplot -p -e "${CMD}"
+done
