a723ba459397d0c106ecf7ce839161beeb8ee12a
Lucas Benedicic <lucas.benedicic@gmail.com>
2013-02-06 10:45:39 +0100

    Added raster and database modes with a command line parameter.-

--- run_worker.sh
+++ run_worker.sh
@@ -11,16 +11,20 @@ if [ "${REDIR}" = "-db" ]; then
     # redirect worker's output to the database server
     #
     ${CMD} | psql -q -h ${PSQL_SERVER} -U ${PSQL_USER} -d ${PSQL_DB}
-else
-    if [ -z "${REDIR}" ]; then
-        ${CMD} > /tmp/worker.log
-        #${CMD} | grep '^[0-9]' - | v.in.ascii -t output=temp format=point -z z=3 --overwrite 
-        #v.to.rast input=temp type=point output=temp use=z --overwrite
+else 
+    if [ "${REDIR}" = "-rast" ]; then
+        ${CMD} | grep '^[0-9]' - | v.in.ascii -t output=temp format=point -z z=3 --overwrite 
+        v.to.rast input=temp type=point output=temp use=z --overwrite
     else
-        echo "Usage: $0 [-db]"
-        echo "Starts a worker process."
-        echo
-        echo " -db	redirects output to the database server, stdout otherwise"
+        if [ -z "${REDIR}" ]; then
+            ${CMD} > /tmp/worker.log
+        else
+            echo "Usage: $0 [-db] [-rast]"
+            echo "Starts a worker process, writing its results to stdout."
+            echo
+            echo " -db		redirects output to the database server, stdout otherwise"
+            echo " -rast	redirects output to a raster map, named 'temp'"
+        fi
     fi
 fi
 
