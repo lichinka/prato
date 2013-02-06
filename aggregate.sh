00cad3db3731ca80c62bb0ac22f2c70059d0abd6
Lucas Benedicic <lucas.benedicic@gmail.com>
2013-02-06 10:40:27 +0100

    Raster resampling removed, because results were incorrect.-

--- aggregate.sh
+++ aggregate.sh
@@ -22,8 +22,6 @@ if [ -n "${TX}" ] && [ -n "${INI}" ] && [ -n "${RAST}" ]; then
     echo -e "\t${SQL}" | psql -q -t -h ${PSQL_SERVER} -U ${PSQL_USER} -d ${PSQL_DB} | tr -d ' ' | v.in.ascii -t output=temp format=point -z z=3 --overwrite
     echo "*** INFO: Converting vector to raster map ..."
     v.to.rast input=temp type=point output=temp use=z --overwrite
-    echo "*** INFO: Changing resolution of the final raster map ..."
-    r.resamp.rst input=temp ew_res=25 ns_res=25 elev=${RAST} --overwrite
     r.colors -n map=${RAST} color=elevation
 else
     echo -e "Usage:\t$0 [comma-separated transmitter's section names] [INI file] [raster name]"
