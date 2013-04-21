a2098de54f86a3e99e6ab96ddc92eb67ff4f4eca
Lucas Benedicic <lucas.benedicic@gmail.com>
2013-04-21 18:01:25 +0200

    Module parameters passed through the script.-

--- run_lte.sh
+++ run_lte.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 OUTPUT=$1
-OPTIONS=$2
-CELLS=$3
+CELLS=$2
+OPTIONS=${@:3}
 
-if [ -n "${OUTPUT}" ] && [ -n "${OPTIONS}" ] && [ -n "${CELLS}" ]; then
+if [ -n "${OUTPUT}" ] && [ -n "${CELLS}" ] && [ -n "${OPTIONS}" ]; then
     if [ "${OUTPUT}" = "-" ]; then
         OUTPUT=" "
     fi
@@ -17,9 +17,11 @@ if [ -n "${OUTPUT}" ] && [ -n "${OPTIONS}" ] && [ -n "${CELLS}" ]; then
     #
     mpirun --mca btl tcp,sm,self -np 1 -host localhost r.coverage ${OPTIONS} ini_file=./parameters_lte.ini tx_ini_sections=${CELLS} :  -np ${NP} --hostfile hostfile.local ./run_worker.sh ${OUTPUT}
 else
-    echo "Usage: $0 [output] [module flags] [comma-separated cell list]"
+    echo "Usage: $0 [output] [comma-separated cell list] [module opts ...]"
     echo "Runs the [r.coverage] tool module for the LTE network with the given parameters.-"
     echo "[output]	May be one of -db, -rast or - for standard output."
+    echo "[cell list]	A comma-separated list of cell names."
+    echo "[module opts]	Any combination of flags and options for the 'r.coverage' GRASS module. Help follows."
     ./r.coverage --help
     exit -1
 fi
